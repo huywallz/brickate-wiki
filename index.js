2c9f15cd1da55e47dd6d258a7368bf493a494f5d
huywallz <huywall69@gmail.com>
2026-04-03 15:26:44 +0700

    nodejs

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Documentation server running on port ${PORT}`);
+});
